c2f7e7e52f78f8f5ac8d14458c6e2bbe2685e612
kinzakhokharr <kinzakhokharr@gmail.com>
2024-08-22 03:39:21 -0700

    deploying my projects

---
+++ basic_01/myapp/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+    </div>
+    
+  );
+}
+export default App;
---
+++ basic_03/myapp3/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-slate-900'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
